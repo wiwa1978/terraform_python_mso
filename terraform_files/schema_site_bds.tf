efeafb0018f7e4514ee8c88f8d089f25f60a1c35
Wim Wauters <wauterw@cisco.com>
2020-11-27 18:37:48 +0100

    Initial Commit

--- terraform_files/schema_site_bds.tf
+++
@@ -1,9 +0,0 @@
-
-resource "mso_schema_site_bd" "sitebdweb" {
-  schema_id       = mso_schema.schSFsch.id
-  bd_name         = mso_schema_template_bd.bdbdweb.name
-  template_name   = mso_schema.schSFsch.template_name
-  site_id         = data.mso_site.site1.id
-  host_route      = true
-}
-
--- terraform_files/sites.tf
+++
@@ -1,8 +0,0 @@
-data "mso_site" "site1" {
-  name  = "SaS_APIC"
-}
-
-data "mso_site" "site2" {
-  name  = "Site2"
-}
-
